090093703d4e2cdddfdf42c3c92ec754696a4bc2
tej-as-kamble <tejaskk13@gmail.com>
2025-11-03 20:41:37 +0530

    fix MirageJS for production

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import './index.css'
 import App from './App.jsx'
 import { makeServer } from "./mirage/server.js";
 
-if (process.env.NODE_ENV === "development") {
-  makeServer({ environment: "development" });
+if (process.env.NODE_ENV === "production") {
+  makeServer({ environment: "production" });
 }
 
 createRoot(document.getElementById('root')).render(
